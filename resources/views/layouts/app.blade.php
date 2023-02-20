e64fd56e5feda72d53378e74e9edaa1353b33d7f
mahnev_sa <mahnev_sa@irgups.ru>
2023-02-20 16:03:34 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,71 +18,73 @@
 </head>
 <body class="font-sans antialiased">
 <div class="container-fluid">
-    @auth
+    @if(\Illuminate\Support\Facades\Auth::user())
         <div class="row">
-        <div class="col-4">
-            <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px;">
-                <img
-                    src="https://www.liveauctioneers.com/news/wp-content/uploads/2021/03/acn-032321-twitter-featured.jpg"
-                    alt="" class="" width="64" height="64">
-                <hr>
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <a href="#" class="btn btn-outline-primary form-control border border-light"
-                           aria-current="page">
-                            <svg class="bi me-2" width="16" height="16">
-                                <use xlink:href="#home"></use>
-                            </svg>
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="btn btn-outline-primary form-control border border-light">
-                            <svg class="bi me-2" width="16" height="16">
-                                <use xlink:href="#speedometer2"></use>
-                            </svg>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="btn btn-outline-primary form-control border border-light">
-                            <svg class="bi me-2" width="16" height="16">
-                                <use xlink:href="#table"></use>
-                            </svg>
-                            Orders
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="btn btn-outline-primary form-control border border-light">
-                            <svg class="bi me-2" width="16" height="16">
-                                <use xlink:href="#grid"></use>
-                            </svg>
-                            Products
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="btn btn-outline-primary form-control border border-light">
-                            <svg class="bi me-2" width="16" height="16">
-                                <use xlink:href="#people-circle"></use>
-                            </svg>
-                            Customers
-                        </a>
-                    </li>
-                </ul>
-                <hr>
-                <a href="" class="btn btn-primary rounded-pill form-control">Твитнуть</a>
+            <div class="col-4">
+                <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px;">
+                    <img
+                        src="https://www.liveauctioneers.com/news/wp-content/uploads/2021/03/acn-032321-twitter-featured.jpg"
+                        alt="" class="" width="64" height="64">
+                    <hr>
+                    <ul class="nav nav-pills flex-column mb-auto">
+                        <li class="nav-item">
+                            <a href="#" class="btn btn-outline-primary form-control border border-light"
+                               aria-current="page">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#home"></use>
+                                </svg>
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-primary form-control border border-light">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#speedometer2"></use>
+                                </svg>
+                                Dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-primary form-control border border-light">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#table"></use>
+                                </svg>
+                                Orders
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-primary form-control border border-light">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#grid"></use>
+                                </svg>
+                                Products
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-outline-primary form-control border border-light">
+                                <svg class="bi me-2" width="16" height="16">
+                                    <use xlink:href="#people-circle"></use>
+                                </svg>
+                                Customers
+                            </a>
+                        </li>
+                    </ul>
+                    <hr>
+                    <a href="" class="btn btn-primary rounded-pill form-control">Твитнуть</a>
+                </div>
+            </div>
+            <div class="col-4">
+                {{ $slot }}
+            </div>
+            <div class="col-4">
+
             </div>
         </div>
-        <div class="col-4">
+    @else
+        <main>
             {{ $slot }}
-        </div>
-        <div class="col-4">
-            
-        </div>
-    </div>
-    @elseauth
-        {{ $slot }}
-    @endauth
+        </main>
+    @endif
 </div>
 
 </body>
